609999e3c23057bce45f8e437d668729e53131d7
Muhsin Ali <muhsin.ali@student.moringaschool.com>
2024-10-05 09:34:17 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+  mainElement.remove();
+}
+
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+
+newHeader.innerHTML = "MUHSIN ALI is the champion";
+
+document.body.append(newHeader);
